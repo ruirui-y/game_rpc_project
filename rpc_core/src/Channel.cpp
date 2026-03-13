8a98598b2ccb5b80f773c5b32c20aede4769abd1
13083361602 <3265868153@qq.com>
2026-03-13 18:02:55 +0800

    RpcChannel

--- rpc_core/src/Channel.cpp
+++ rpc_core/src/Channel.cpp
@@ -1,12 +1,14 @@
 #include "Channel.h"
 #include <string>
-#include "rpcheader.pb.h"
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
 #include <mymuduo/Log/Logger.h>
+
+#include "rpcheader.pb.h"
+#include "ConnectionPool.h"
 
 void Channel::CallMethod(const google::protobuf::MethodDescriptor* method,
     google::protobuf::RpcController* controller,
@@ -26,21 +28,93 @@ void Channel::CallMethod(const google::protobuf::MethodDescriptor* method,
     header.set_method_name(method->name());
     header.set_args_size(param_size);
 
-    // 3. 序列化请求头
+    // 3. 序列化请求头和请求数据
     std::string header_str;
     header.SerializeToString(&header_str);
     uint32_t header_size = header_str.size();
 
-    // 4. 序列化请求数据
     std::string request_str;
     request->SerializeToString(&request_str);
 
-    // 5. 发送数据
-    int len = sizeof(uint32_t) + header_str.size() + request_str.size();
-    char buf[len];
-    memcpy(buf, &header_size, sizeof(int));
-    memcpy(buf, header_str.c_str(), header_str.size());
-    memcpy(buf + header_str.size(), request_str.c_str(), request_str.size());
+    // 5. 数据整合
+    std::string send_rpc_str;
+    send_rpc_str.insert(0, std::string((char*)&header_size, 4));
+    send_rpc_str += header_str;
+    send_rpc_str += request_str;
 
-    // 复用连接
+    // 6. 复用连接并发送数据
+    int sockfd = ConnectionPool::GetInstance().GetConnection("127.0.0.1", 9090);
+    if (-1 == send(sockfd, send_rpc_str.c_str(), send_rpc_str.size(), 0))                             // 发送二进制流
+    {
+        close(sockfd);                                                                                // 失败关闭
+        if (controller) controller->SetFailed("send error! errno:" + std::to_string(errno));          // 记录失败
+        return;
+    }
+
+    // 7. 接收响应头
+    uint32_t response_size;
+    int offset = 0;
+    while (offset < 4)
+    {
+        int n = recv(sockfd, (char*)&response_size + offset, 4 - offset, 0);
+        if (n <= 0)
+        {
+            // 连接可能断了，按你之前的逻辑处理
+            close(sockfd);
+            return;
+        }
+        offset += n;
+    }
+
+    if (response_size > 10 * 1024 * 1024)
+    {
+        if (controller) controller->SetFailed("response is too big!");
+        close(sockfd);
+        return;
+    }
+
+
+    // 8. 接收响应体
+    std::string response_data;
+    response_data.resize(response_size);
+    uint32_t total_recv = 0;
+
+    // 设置超时事件
+    struct timeval timeout = { 5, 0 };
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(struct timeval));
+    while (total_recv < response_size)
+    {
+        // 往指定的位置写入指定大小的数据
+        int n = recv(sockfd, &response_data[total_recv], response_size - total_recv, 0);
+        // 超时
+        if (n < 0)
+        {
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+            {
+                if (controller) controller->SetFailed("RPC recv timeout!");
+            }
+            else
+            {
+                if (controller) controller->SetFailed("recv error! errno:" + std::to_string(errno));
+            }
+            close(sockfd);
+            return;
+        }
+        // 连接断开
+        else if (n == 0)
+        {
+            if (controller) controller->SetFailed("server connection closed unexpectedly!");
+            close(sockfd);
+            return;
+        }
+        total_recv += n;
+    }
+
+    // 9. 反序列化响应数据
+    if (!response->ParseFromString(response_data))                                                      // 解析并填充
+    {
+        close(sockfd);                                                                                  // 失败关闭
+        if (controller) controller->SetFailed("parse error! response_str:" + response_data);            // 解析失败
+        return;
+    }
 }
