04d927a6bf26999a270ba0e08a9b1af988bc5963
13083361602 <3265868153@qq.com>
2026-03-14 15:39:36 +0800

    添加端口和ip，以便获取对应的连接

--- rpc_core/include/MyChannel.h
+++ rpc_core/include/MyChannel.h
@@ -4,14 +4,24 @@
 #include <google/protobuf/service.h>
 #include <google/protobuf/descriptor.h>
 #include <google/protobuf/message.h>
+#include <string>
 
 class MyChannel : public google::protobuf::RpcChannel
 {
+public:
+    MyChannel(const std::string& ip, int port)
+        : ip_(ip), port_(port) {}
+    ~MyChannel() {}
+
 public:
     void CallMethod(const google::protobuf::MethodDescriptor* method,
         google::protobuf::RpcController* controller,
         const google::protobuf::Message* request,
         google::protobuf::Message* response,
         google::protobuf::Closure* done);
+
+private:
+    std::string ip_;
+    int port_;
 };
 #endif
