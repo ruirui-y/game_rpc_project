1fc18eca34c0d88d311a8c5cd881e8f6c04e7c2a
13083361602 <3265868153@qq.com>
2026-03-14 11:48:18 +0800

    本地字节序到网络字节序的转换

--- rpc_core/src/Channel.cpp
+++ rpc_core/src/Channel.cpp
@@ -38,43 +38,65 @@ void Channel::CallMethod(const google::protobuf::MethodDescriptor* method,
     std::string header_str;
     header.SerializeToString(&header_str);
     uint32_t header_size = header_str.size();
+    uint32_t net_header_size = htonl(header_size);                                                      // 发送网络字节序
 
     std::string request_str;
     request->SerializeToString(&request_str);
 
     // 5. 数据整合
-    std::string send_rpc_str;
-    send_rpc_str.insert(0, std::string((char*)&header_size, 4));
+    std::string send_rpc_str;;                                                      
+    send_rpc_str.insert(0, std::string((char*)&net_header_size, 4));
     send_rpc_str += header_str;
     send_rpc_str += request_str;
 
     // 6. 复用连接并发送数据
     int sockfd = ConnectionPool::GetInstance().GetConnection("127.0.0.1", 9090);
-    if (-1 == send(sockfd, send_rpc_str.c_str(), send_rpc_str.size(), 0))                             // 发送二进制流
+    if (-1 == send(sockfd, send_rpc_str.c_str(), send_rpc_str.size(), 0))                               // 发送二进制流
     {
-        close(sockfd);                                                                                // 失败关闭
-        if (controller) controller->SetFailed("send error! errno:" + std::to_string(errno));          // 记录失败
+        close(sockfd);                                                                                  // 失败关闭
+        if (controller) controller->SetFailed("send error! errno:" + std::to_string(errno));            // 记录失败
         return;
     }
 
-    // 7. 接收响应头
-    uint32_t response_size;
+    // 设置超时时间
+    struct timeval timeout = { 5, 0 };
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(struct timeval));
+    
+    // 7. 接收响应头(4字节)
+    uint32_t network_response_size = 0; // 用来存网络字节序的数据
     int offset = 0;
     while (offset < 4)
     {
-        int n = recv(sockfd, (char*)&response_size + offset, 4 - offset, 0);
-        if (n <= 0)
+        int n = recv(sockfd, (char*)&network_response_size + offset, 4 - offset, 0);
+        if (n < 0)
         {
-            // 连接可能断了，按你之前的逻辑处理
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+            {
+                if (controller) controller->SetFailed("RPC recv header timeout!");
+            }
+            else
+            {
+                if (controller) controller->SetFailed("recv header error! errno:" + std::to_string(errno));
+            }
+            close(sockfd);
+            return;
+        }
+        else if (n == 0)
+        {
+            // 连接断开
+            if (controller) controller->SetFailed("server connection closed unexpectedly while reading header!");
             close(sockfd);
             return;
         }
         offset += n;
     }
 
+    // 转换成本地主机字节序
+    uint32_t response_size = ntohl(network_response_size);
+
     if (response_size > 10 * 1024 * 1024)
     {
-        if (controller) controller->SetFailed("response is too big!");
+        if (controller) controller->SetFailed("response is too big! size: " + std::to_string(response_size));
         close(sockfd);
         return;
     }
@@ -85,31 +107,27 @@ void Channel::CallMethod(const google::protobuf::MethodDescriptor* method,
     response_data.resize(response_size);
     uint32_t total_recv = 0;
 
-    // 设置超时事件
-    struct timeval timeout = { 5, 0 };
-    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(struct timeval));
     while (total_recv < response_size)
     {
         // 往指定的位置写入指定大小的数据
         int n = recv(sockfd, &response_data[total_recv], response_size - total_recv, 0);
-        // 超时
-        if (n < 0)
+
+        if (n < 0)                                                                                                  // 发生错误或超时
         {
             if (errno == EAGAIN || errno == EWOULDBLOCK)
             {
-                if (controller) controller->SetFailed("RPC recv timeout!");
+                if (controller) controller->SetFailed("RPC recv body timeout!");
             }
             else
             {
-                if (controller) controller->SetFailed("recv error! errno:" + std::to_string(errno));
+                if (controller) controller->SetFailed("recv body error! errno:" + std::to_string(errno));
             }
             close(sockfd);
             return;
         }
-        // 连接断开
-        else if (n == 0)
+        else if (n == 0)                                                                                            // 连接断开
         {
-            if (controller) controller->SetFailed("server connection closed unexpectedly!");
+            if (controller) controller->SetFailed("server connection closed unexpectedly while reading body!");
             close(sockfd);
             return;
         }
@@ -117,10 +135,10 @@ void Channel::CallMethod(const google::protobuf::MethodDescriptor* method,
     }
 
     // 9. 反序列化响应数据
-    if (!response->ParseFromString(response_data))                                                      // 解析并填充
+    if (!response->ParseFromString(response_data))                                                                  // 解析并填充
     {
-        close(sockfd);                                                                                  // 失败关闭
-        if (controller) controller->SetFailed("parse error! response_str:" + response_data);            // 解析失败
+        close(sockfd);                                                                                              // 失败关闭
+        if (controller) controller->SetFailed("parse error! response_str:" + response_data);                        // 解析失败
         return;
     }
 }
