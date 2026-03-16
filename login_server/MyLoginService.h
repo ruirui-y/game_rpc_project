bf6bb5bcd2667466c1c5ae01dccfdaf1db3b3852
13083361602 <3265868153@qq.com>
2026-03-16 17:27:16 +0800

    添加分布式锁

--- login_server/MyLoginService.h
+++ login_server/MyLoginService.h
@@ -3,19 +3,13 @@
 
 #include <string>
 #include "login.pb.h"
+#include "RedisClient.h"
 #include <mymuduo/Log/Logger.h>
 using namespace game::rpc;
 
 class MyLoginService : public game::rpc::LoginService
 {
 public:
-    // 真正的本地业务逻辑
-    bool Login(std::string name, std::string pwd)
-    {
-        LOG_INFO << "Execute Login Service";
-        LOG_INFO << "user_name = " << name.c_str() << " pwd = " << pwd.c_str();
-        return true;
-    }
 
     virtual void Login(::google::protobuf::RpcController* controller,
         const ::LoginRequest* request,
@@ -26,16 +20,36 @@ public:
         std::string name = request->username();
         std::string pwd = request->password();
 
-        // 2. 执行本地业务
-        bool login_result = Login(name, pwd);                                                       
+        RedisClient redis;
+        if (redis.Connect("127.0.0.1", 6379) == false)
+        {
+            response->set_errcode(500);
+            response->set_errmsg("redis connect error");
+            if (done) done->Run();
+            return;
+        }
+
+        // 2. 分布式锁防御
+        std::string lock_key = "lock:login:" + name;
+        // 尝试加锁，过期时间为3s
+        if (!redis.SetNx(lock_key, "1", 3))
+        {
+            response->set_errcode(403);
+            response->set_errmsg("server is busy, please try again later");
+            if (done) done->Run();
+            return;
+        }
+
+        // 2. 执行本地业务todo                                                  
 
         // 3. 把响应写回给框架
-        response->set_errcode(login_result == true ? 0 : 1);
-        response->set_errmsg("");                                                                   
+        response->set_errcode(0);
+        response->set_errmsg("登录成功");
         response->set_token("11111111");
 
-        // 4. 执行回调操作
+        // 4. 释放分布式锁并执行回调操作
         // 这里的 done 本质是 RpcProvider 传进来的 SendRpcResponse 回调
+        redis.Del(lock_key);
         done->Run();                                                                                
     }
 };
