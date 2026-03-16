29fcbf028494ba2892391791000d9ae10473da09
13083361602 <3265868153@qq.com>
2026-03-16 18:55:07 +0800

    获取目标ID的网关

--- chat_server/ChatServiceImpl.cpp
+++ chat_server/ChatServiceImpl.cpp
@@ -1,6 +1,8 @@
 #include "ChatServiceImpl.h"
 #include <mymuduo/Log/Logger.h>
 #include "RedisClient.h"
+#include "MyChannel.h"
+#include "gateway.pb.h"
 
 game::rpc::ChatServiceImpl::ChatServiceImpl()
 {
@@ -37,11 +39,42 @@ void game::rpc::ChatServiceImpl::SendMessage(::google::protobuf::RpcController* 
 		}
 		else
 		{
-			// todo: 构建一个RPC客户端，通过调用网关的PushMessage(RPC方法)，将消息转发回去
-            response->set_errcode(0);
-			response->set_errmsg("Send Success");
-			LOG_INFO << "ChatServer Send Success To Target UID: " << target_id
-				<< "Location Gateway: " << target_gateway;
+			// 构建一个RPC客户端，通过调用网关的PushMessage(RPC方法)，将消息转发回去
+			// 解析出 IP 和 端口
+			std::string ip = target_gateway.substr(0, target_gateway.find(':'));
+			int port = std::stoi(target_gateway.substr(target_gateway.find(':') + 1));
+
+			// 初始化RPC通道
+			MyChannel channel(ip, port);
+			GatewayService_Stub stub(&channel);
+
+			// 组装发给网关的推送请求
+			PushMessageRequest push_req;
+			push_req.set_user_id(target_id);
+			push_req.set_msg_type(3003);
+			push_req.set_content(message);
+
+			// 响应
+			PushMessageResponse push_res;
+			
+			// 发起RPC调用
+			stub.PushMessage(nullptr, &push_req, &push_res, nullptr);
+
+			// 消息路由成功
+			if (push_res.errcode() == 0)
+			{
+				response->set_errcode(0);
+				response->set_errmsg("Send Success");
+				LOG_INFO << "ChatServer Send Success To Target UID: " << target_id
+					<< "Location Gateway: " << target_gateway;
+			}
+			else
+			{
+				response->set_errcode(500);
+				response->set_errmsg("Gateway PushMessage Failed");
+                LOG_INFO << "ChatServer Send Failed To Target UID: " << target_id
+					<< "Location Gateway: " << target_gateway;
+			}
 		}
 	}
 	// 群聊
